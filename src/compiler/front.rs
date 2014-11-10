1411b9221c36b57346e104877848e1e18384c8f9
Alex Iadicicco <alex@ajitek.net>
2014-11-10 01:45:18 -0700

    Actually include 'front.rs'

---
+++ src/compiler/front.rs
@@ -0,0 +1,94 @@
+/* converts parse tree to IR */
+
+use compiler::tree::*;
+use compiler::ir::*;
+
+pub fn opn_to_value<'a, 'b>(
+    opn: &'a Operand,
+    bb:  &'b BasicBlock<'b>
+) -> Value<'b> {
+    match *opn {
+        Operand::Expression(opr, box ref opn0, box ref opn1) => {
+            let v0 = opn_to_value(opn0, bb);
+            let v1 = opn_to_value(opn1, bb);
+
+            InstResult(bb.add_inst(Instruction {
+                opr: match opr {
+                    Operator::Add       => OpAdd,
+                    Operator::Subtract  => OpSubtract,
+                    Operator::Multiply  => OpMultiply,
+                    Operator::Divide    => OpDivide,
+                },
+
+                opn: vec!(v0, v1),
+            }))
+        },
+
+        Operand::Constant(c) =>
+            Constant(c),
+
+        Operand::Identifier(ref id) =>
+            Location(id.clone()),
+
+        Operand::Nothing => {
+            println!("warning: encountered Operand::Nothing");
+            Constant(0)
+        }
+    }
+}
+
+pub fn add_stmt_to_cfg<'a, 'b>(
+    st:  &'a Statement,
+    bb:  &'b BasicBlock<'b>
+) -> &'b BasicBlock<'b> {
+    match *st {
+        Statement::Print(ref opn) => {
+            bb.add_inst(Instruction {
+                opr: OpPrint,
+                opn: vec!(opn_to_value(opn, bb)),
+            });
+            return bb;
+        },
+
+        Statement::IfBlock(ref opn, box ref stmts) => {
+            bb.add_inst(Instruction {
+                opr: OpTestNonzero,
+                opn: vec!(opn_to_value(opn, bb))
+            });
+            let fb = bb.function.add_block();
+            let tb = bb.function.add_block();
+            add_stmts_to_cfg(stmts, tb);
+            tb.set_true(Some(fb));
+            bb.set_true(Some(fb));
+            return fb;
+        },
+
+        Statement::Assign(ref to, ref opn) => {
+            bb.add_inst(Instruction {
+                opr: OpAssign,
+                opn: vec!(
+                    Location(to.clone()),
+                    opn_to_value(opn, bb)
+                ),
+            });
+            return bb;
+        },
+    }
+}
+
+pub fn add_stmts_to_cfg<'a, 'b>(
+    st:  &'a StatementList,
+    bb:  &'b BasicBlock<'b>
+) -> &'b BasicBlock<'b> {
+    let mut exit = bb;
+
+    for cur in st.iter() {
+        exit = add_stmt_to_cfg(&**cur, exit);
+    }
+
+    return exit;
+}
+
+pub fn stmt_list_to_ir<'a, 'b>(st: &'a StatementList, f: &'b Function<'b>) {
+    add_stmts_to_cfg(st, f.add_block());
+}
