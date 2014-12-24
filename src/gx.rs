cec7e673e88afaf7f8eaee7ba05c8a03a4dc7ee3
Alex Iadicicco <alex@ajitek.net>
2014-12-24 00:52:16 -0700

    Remove a bunch of crap that's not being used.

--- src/gx.rs
+++ src/gx.rs
@@ -8,73 +8,14 @@ mod driver {
 
     use std::io::stdin;
     use compiler::tree::*;
-    use compiler::ir;
     use compiler::lexer;
     use compiler::parser;
-    use compiler::front;
 
     fn parse<B: Buffer>(mut f: B) -> StatementList {
         parser::Parser::new(lexer::Lexer::new(f.chars())).stmt_list()
     }
 
-    fn dump_bb(bb: &ir::BasicBlock) {
-        println!("block{}:", bb.num);
-
-        for x in bb.insts.borrow().iter() {
-            match x.opr {
-                ir::OpAdd          => print!("  %{} = add", x.num),
-                ir::OpSubtract     => print!("  %{} = sub", x.num),
-                ir::OpMultiply     => print!("  %{} = mul", x.num),
-                ir::OpDivide       => print!("  %{} = div", x.num),
-
-                ir::OpPrint        => print!("  print"),
-                ir::OpTestNonzero  => print!("  test"),
-                ir::OpAssign       => print!("  assign"),
-            }
-
-            for v in x.opn.iter() {
-                match *v {
-                    ir::Constant(c)     => print!(" {}", c),
-                    ir::Location(ref s) => print!(" {}", s),
-                    ir::InstResult(i)   => print!(" %{}", i.num),
-                }
-            }
-
-            print!("\n");
-        }
-
-        let tb = bb.on_true.get();
-        let fb = bb.on_false.get();
-
-        match (tb, fb) {
-            (Some(tbb), Some(fbb)) => {
-                println!("  bf block{}", fbb.num);
-                dump_bb(tbb)
-            },
-
-            (None,      Some(fbb)) => {
-                println!("  bf block{}", fbb.num);
-                println!("  exit");
-            },
-
-            (Some(tbb), None     ) => {
-                dump_bb(tbb)
-            },
-
-            (None,      None     ) => {
-                println!("  exit");
-            },
-        }
-    }
-
-    fn dump_ir(f: &ir::Function) {
-        match f.get_entry() {
-            Some(bb) => dump_bb(bb),
-            None => println!("function has no entry BasicBlock!"),
-        }
-    }
-
-    pub fn main(args: Vec<String>) {
+    pub fn main() {
         /*
         let matches = match getopts::getopts(args.tail(), [
             getopts::optopt("o", "", "set output file name", "NAME"),
@@ -91,17 +32,10 @@ mod driver {
 
         /* read a statement list from the stream */
         let st = parse(stdin());
-
-        /* build IR */
-        let f = ir::Function::new();
-        front::stmt_list_to_ir(&st, &f);
-
-        /* dump IR */
-        dump_ir(&f);
     }
 
 }
 
 fn main() {
-    driver::main(std::os::args());
+    driver::main();
 }
