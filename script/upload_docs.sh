0604ae0061eee18e40c88e4e25801b65af6afed9
Alex Iadicicco <alex@ajitek.net>
2015-01-04 02:26:45 -0700

    Make upload_docs.sh better

--- script/upload_docs.sh
+++ script/upload_docs.sh
@@ -2,6 +2,5 @@
 
 cd $(git rev-parse --show-toplevel || exit 1)
 cargo doc || exit 1
-find target/doc -print0 | xargs -0 chmod a+r || exit 1
-find target/doc -type d -print0 | xargs -0 chmod a+x || exit 1
-rsync -aP target/doc/ gx@gx:gx/target/doc
+chmod -R a+rX target/doc || exit 1
+rsync -a --info=progress2 target/doc/ gx@gx:gx/target/doc
